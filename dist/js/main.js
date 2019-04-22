3f985f6a33729b935fff9e7440765e3bbfcadc6b
Matt Hoffman <formatt.hoffman@gmail.com>
2019-04-22 10:29:48 -0700

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,17 +14,17 @@ function toogleMenu() {
   //console.log(showMenu);
   if (!showMenu) {
     menuBtn.classList.add("close");
-    menu.classList.add("close");
-    menuNav.classList.add("close");
-    menuBranding.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
     // Set menu state
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
-    menu.classList.remove("close");
-    menuNav.classList.remove("close");
-    menuBranding.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
     // Set menu state
     showMenu = false;
